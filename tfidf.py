97feef52dbe5e656efc02608a58b0c00323ac0af
tunhsiang <6639fv@gmail.com>
2021-12-30 00:33:03 +0800

    delete files

--- tfidf.py
+++
@@ -1,9 +0,0 @@
-from gensim import models
-from docRetrieval import DocRetrieval
-import utils
-
-rtv = DocRetrieval('train')
-tfidf = models.TfidfModel(rtv.corpus)
-ans = rtv.retrieve(tfidf)
-utils.write_file(ans, 'submission.csv')
-utils.map_score('train_ans.csv', 'submission.csv')
